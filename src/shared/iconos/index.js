c221427ed2c1f548e388c8fb26daf8ed5ac59ad8
Andrea Niño <167582509+AndreaBedoya@users.noreply.github.com>
2025-04-23 10:01:02 -0500

    actualizacion de iconos sidebar

---
+++ src/shared/iconos/index.js
@@ -0,0 +1,12 @@
+// INDEX DE LOS ICONOS
+export { default as BarraDeslizada } from "./BarraDeslizadaIcon";
+export { default as BarraLateral} from "./BarraLateralIcon";
+export { default as Clientes } from "./ClientesIcon";
+export { default as Fabricantes } from "./FabricantesIcon";
+export { default as Home } from "./InicioIcon";
+export { default as Productos } from "./ProductosIcon";
+export { default as Reporte } from "./ReporteIcon";
+export { default as Estadisticas } from "./EstadisticasIcon";
+export { default as Usuarios } from "./UsuariosIcon";
+export { default as Perfil } from "./PerfilIcon";
+export { default as CerrarSesion } from "./CerrarSesionIcon";
