14a227fe0e35c7a1a04ef1dac7d9175779af0cfc
DavidFelipe-dev <davidfgonzalezc1@gmail.com>
2025-05-09 19:47:40 -0500

    FEAT: Se implementa la proteccion de rutas entre roles

--- src/middleware/require-auth.js
+++ src/middleware/require-auth.js
@@ -6,13 +6,25 @@ export async function requireAuth(context, next) {
     const authCookie = request.headers.get("cookie")
 
     // Rutas publicas que no requieren autenticacion
-    const publicRoutes = ["/", "/login", "/contrasena"]
+    const publicRoutes = ["/", "/login", "/contrasena", "/unauthorized"]
+
+    // Acceso a las rutas privadas por rol
+    const accessControl = {
+        admin: ["/dashboard", "/usuarios", "/productos", "/clientes"],
+        analista: ["/dashboard", "/productos", "/clientes"]
+    }
     const url = new URL(request.url)
 
     // Verificamos si la ruta es publica
     const isPublic = publicRoutes.some(route => url.pathname === route || url.pathname.startsWith("/api/"))
+    // Si la ruta es publica se salta las validaciones
+    if (isPublic) {
+        return next()
+    }
 
-    let isValid = false
+    let isValid = false // Variable para verificar si el token es valido
+    let userRole = null // Variable para guardar el rol del usuario
+
     // Obtenemos la cookie de autenticacion y verificamos
     try {
         const response = await api.get("/validar-token", {
@@ -22,19 +34,35 @@ export async function requireAuth(context, next) {
         })
 
         isValid = response.data.data.valid
-
-    } catch (error) {
+        userRole = response.data.data.rol
+    
+    } 
+    // En caso de haber un error se hace un log en la consola del servidor
+    catch (error) {
         console.log(error)
     }
 
     // ----------------
     // Validaciones
     // ----------------
-    if(isValid && url.pathname === "/login") {
+
+    // Si ya se autentico e intenta ir a lofin, redirige a su dashboard
+    if (isValid && url.pathname === "/login") {
         return redirect("/dashboard")
     }
 
-    if(isPublic || isValid){
+    // Verificamos que el usuario autenticado tenga acceso a la ruta
+    if (isValid) {
+        // Obtenemos las rutas a las que el usuario tiene aceso
+        const allowedRoutes = accessControl[userRole] || []
+
+        // Verificar si la ruta actual esta permitida para el usuario
+        const hasAccess = allowedRoutes.some(route => url.pathname.startsWith(route))
+
+        //Redirigimos en caso de que el usuario no tenga acceso
+        if (!hasAccess) {
+            return redirect("/unauthorized")
+        }
         return next()
     }
 
