f44d0e084e7b220b3ebd06fb3ffad991c56e57e9
DavidFelipe-dev <davidfgonzalezc1@gmail.com>
2025-05-20 21:58:23 -0500

    FEAT: Vista de listado de producto con filtros, barra de busqueda y borrado rapido

--- src/features/productos/services/index.js
+++ src/features/productos/services/index.js
@@ -10,13 +10,13 @@ export const RegistrarProducto = async (data) => {
   try {
     await api.post("/productos/crear", data)
     return true
-  } 
+  }
 
 
   catch (err) {
     // Si hay respuesta del servidor
     console.log(err)
-    switch(err.response.status) {
+    switch (err.response.status) {
       case HttpStatusCode.UnprocessableEntity:
         throw new Error(err.response.data.error);
       case HttpStatusCode.Conflict:
@@ -31,22 +31,41 @@ export const RegistrarProducto = async (data) => {
 export const GetRegistrosEntradaProducto = async () => {
 
   // Se hace el llamado a la api
-  try{
+  try {
     const response = await api.get("/registroEntradaProductos")
-    console.log(response.data.data)
-    if(response.data.data) {
+    if (response.data.data) {
       return response.data.data
     }
     return []
   }
 
   // En caso de errores se manejan los errores
-  catch(err){
+  catch (err) {
     // Devolvemos un mensaje de error dependiendo del estado de la peticion
-    switch(err.response.status){
+    switch (err.response.status) {
       case HttpStatusCode.InternalServerError:
         throw new Error("No eres tu somo nosotros, tenemos problemas de conexion vuelve a intentarlo mas tarde!")
     }
   }
 
+}
+
+// Este servicio nos permite obtener eliminar un producto en la base de datos
+export const deleteProducto = async (numeroRegistroProducto) => {
+
+  // Se hace el llamado a la api
+  try {
+    await api.delete(`/productos/${numeroRegistroProducto}`)
+    return true
+  }
+
+  // En caso de errores se envia un mensaje personalizado dependiendo del estado devuelto
+  catch (err) {
+    switch (err.response.status) {
+      case HttpStatusCode.NotFound:
+        throw new Error("Este producto ya ha sido eliminado, intenta recargar la pagina si lo sigues viendo")
+      case HttpStatusCode.InternalServerError:
+        throw new Error("No eres tu comos nosotros, hubo en error de nuestra parte vuelve a intentarlo!")
+    }
+  }
 }
