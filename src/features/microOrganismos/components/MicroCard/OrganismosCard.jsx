4556bdfcb6c3caea75695b9752d218e8d4b8c06f
santiago-Quitian <santyq2003@gmail.com>
2025-06-19 16:29:18 -0500

    feat:se agrega el crud de el submodulo de microOrganismos

---
+++ src/features/microOrganismos/components/MicroCard/OrganismosCard.jsx
@@ -0,0 +1,117 @@
+import { useState } from "react"
+import {Button, Modal } from "@shared/components"
+import { EditIcon, TrashIcon,ArrowBackIcon } from "@shared/iconos"
+import { EditarOrganismos} from '../editMicro/EditarOrganismo'
+import Swal from "sweetalert2";
+import {  EliminarOrganismo } from "../../services"
+import styles from "./OrganismosCard.module.css"
+
+export function OrganismoCard({ Organismo }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleDelete = () => {
+    Swal.fire({
+      title: "¿Estás seguro?",
+      text: "Esta acción eliminará el Organismo de forma permanente.",
+      icon: "question",
+      showCancelButton: true,
+      cancelButtonText: "Cancelar",
+      confirmButtonText: "Aceptar",
+      heightAuto: false,
+      scrollbarPadding: false,
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          await  EliminarOrganismo(Organismo.id);
+          Swal.fire({
+            title: "¡Se eliminó el Organismo con éxito!",
+            icon: "success",
+            heightAuto: false,
+            scrollbarPadding: false,
+          }).then(() => {
+            window.location.href = "/productos";
+          });
+        } catch (err) {
+          Swal.fire({
+            icon: "error",
+            title: "Hubo un error al eliminar el Organismo",
+            text: err.message,
+            heightAuto: false,
+            scrollbarPadding: false,
+          });
+        }
+      }
+    });
+  };
+
+  return (
+    <div>
+        <Button variant={"buttonCancel"} parentMethod={() => window.location.href =`productos/${numeroRegistroProducto}`}>
+          Atras
+          <ArrowBackIcon />
+        </Button>
+
+      <p className={styles.nuemroR}><span className={styles.texto}>Numero registro producto: </span>{Organismo.numeroRegistroProducto}</p>
+      <div className={styles.infoContainer}>
+        <div className="row">
+
+          <div className="col-lg-5">
+            <p className={styles.contenido}>
+              <span className={styles.texto}>Nombre Prueba: </span>
+              <br />
+              {Organismo.nombreMicroorganismo}
+            </p>
+            <p className={styles.contenido}>
+              <span className={styles.texto}>Cantidad Muestra: </span>
+              <br />
+              {Organismo.cantidadMuestra}
+            </p>
+            <div>
+              <p className={styles.contenido}>
+                <span className={styles.texto}>Método: </span>
+                <br />
+                {Organismo.metodoUsado}
+              </p>
+              <p className={styles.contenido}>
+                <span className={styles.texto}>Especificación: </span>
+                <br />
+                {Organismo.especificacion}
+              </p>
+            </div>
+          </div>
+
+          <div className="col-lg-5">
+            <p className={styles.contenido}>
+              <span className={styles.texto}>Tratamiento: </span>
+              <br />
+              {Organismo.tratamiento} 
+            </p>
+            <p className={styles.contenido}> 
+              <span className={styles.texto}>Volumen Diluyente: </span>
+              <br />
+              {Organismo.volumenDiluyente}
+            </p>
+          </div>
+        </div>
+
+        <div className={`${styles.buttonContainer} mt-6`}>
+          <Button variant="buttonCancel" parentMethod={handleDelete}>
+            Eliminar <TrashIcon />
+          </Button>
+
+          <Button variant="buttonEdit" parentMethod={() => setIsModalOpen(true)}>
+            Editar <EditIcon />
+          </Button>
+        </div>
+
+        <Modal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          title="Editar Organismo"
+        >
+          <EditarOrganismos data={Organismo} onClose={() => setIsModalOpen(false)} />
+        </Modal>
+      </div>
+    </div>
+  );
+}
---
+++ src/features/microOrganismos/services/index.js
@@ -0,0 +1,78 @@
+import api from "@shared/services/api.js"
+import { HttpStatusCode } from "axios"
+
+// ESTE SERVCIO PERMITE REGISTRAR UNA PRUEBA DE RECUENTO DE  PRODUCTO HACIENDO USO DE LA API
+export const RegistrarOrganismo = async (data) => {
+  // SE HACE EL LLAMADO A LA API
+  try {
+    await api.post("/deteccionMicroorganismos", data)
+    return true
+  } 
+  catch (err) {
+    // Si hay respuesta del servidor
+    console.log(err)
+    switch(err.response.status) {
+      case HttpStatusCode.UnprocessableEntity:
+        throw new Error(err.response.data.error);
+      case HttpStatusCode.Conflict:
+        throw new Error(err.response.data.error)
+      default:
+        throw new Error("Error en el servidor, vuelve a intentarlo más tarde");
+    }
+  }
+}
+
+// Este servicio permite editar una prueba de recuento 
+export const EditOrganismo = async (id, data) => {
+  try {
+    const response = await api.put(`/deteccionMicroorganismos/${id}`, data)
+    // Validar que efectivamente devolvió algo útil
+    if (response.status === 200) {
+      return response.data
+    } else {
+      throw new Error("No se pudo actualizar el recuento correctamente.")
+    }
+
+  } catch (err) {
+    console.log(err)
+    switch (err.response?.status) {
+      case HttpStatusCode.UnprocessableEntity:
+        throw new Error(err.response.data.error)
+      case HttpStatusCode.NotFound:
+        throw new Error("El recuento que estás intentando editar no existe.");
+      default:
+        throw new Error("Error en el servidor, vuelve a intentarlo más tarde.");
+    }
+  }
+}
+// Este servicio permite visionar una prueba de recuento por id
+export const VistaOrganismosID = async (id)=>{
+  try {
+    const response = await api.get(`deteccionMicroorganismos/${id}`)
+    return response.data.data
+  }  catch (err) {
+    switch (err.response.status) {
+      case HttpStatusCode.UnprocessableEntity:
+        throw new Error(err.response.data.error)
+      case HttpStatusCode.NotFound:
+        throw new Error("El Micro Organismo que estas intentando visionar no existe");
+      default:
+        throw new Error("Error en el servidor, vuelve a intentarlo más tarde");
+    }
+    }
+}
+
+// Este servicio permite eliminar el recuento
+export const EliminarOrganismo =async (id) => {
+  try {
+    await api.delete(`/deteccionMicroorganismos/${id}`)
+    return true
+  } catch (err) {
+     switch(err.response.status) {
+            case HttpStatusCode.NotFound:
+                throw new Error("El Micro Organismo ya ha sido eliminado de la base de datos del sistema, intenta recargar la pagina para reflejar los cambios!")
+            case HttpStatusCode.Conflict:
+                throw new Error("Este Micro organismos tiene actualmente productos relacionados, por ende no se puede borrar.")
+        }
+  }
+}
